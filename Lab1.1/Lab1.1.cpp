8131e337220a49e97629cf51f9bc38675c30f940
Kurin, Alex <olexkurin@gmail.com>
2021-09-11 14:32:28 +0300

    lab 1.1

---
+++ Lab1.1/Lab1.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#include "Lab1.1.h"
+#include "Point.h"
+
+double CalculateDistance(Point A, Point B)
+{
+	return sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y, 2));
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	Point A = EnterPoint("A");
+	Point B = EnterPoint("B");
+
+	std::cout << "Відстань між точками A та B: " << CalculateDistance(A, B) << std::endl;
+
+	std::cin.get();
+}
---
+++ Lab1.1/helpers.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#include "helpers.h"
+
+double EnterDecimal(const char* decimal_name)
+{
+	double number;
+
+	bool isFailure = true;
+	do
+	{
+		std::cout << "\tВведіть " << decimal_name << ": ";
+		std::cin >> number;
+		isFailure = std::cin.fail();
+
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
+	} while (isFailure);
+
+	return number;
+}
