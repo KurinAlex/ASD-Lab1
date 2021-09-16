6d7b6169293c9d1d80a9091ed28579e351870430
Kurin, Alex <olexkurin@gmail.com>
2021-09-16 17:18:40 +0300

    lab1.4 first commit

---
+++ Lab1.4/Lab1.4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+
+#define ARRAY_SIZE 16
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+
+	srand(time(0));
+
+	int array[ARRAY_SIZE];
+	int half_size = ARRAY_SIZE / 2;
+	int max = -RAND_MAX;
+	int diff;
+
+	std::cout << "Масив: [ ";
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		array[i] = rand();
+		std::cout << array[i] << ' ';
+	}
+	std::cout << "]\n";
+
+	for (int i = 0; i < half_size; i++)
+	{
+		diff = array[i] - array[i + half_size];
+		if (diff > max)
+		{
+			max = diff;
+		}
+	}
+
+	std::cout << "Max: " << max;
+
+	std::cin.get();
+}
